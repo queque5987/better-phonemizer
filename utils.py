41c7b75ec871ba9c25b26509e66e6797560c5ef9
Young-woong Park <64758868+queque5987@users.noreply.github.com>
2022-08-25 20:24:04 +0900

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,6 +3,6 @@ import espeak_phonemizer
 
 def text_to_phoneme(transcription, is_stress=False):
     assert type(transcription) == str
-    pmzr = espeak_phonemizer.phonemizer()
+    pmzr = espeak_phonemizer.Phonemizer()
     phoneme = pmzr.phonemize(transcription).rstrip()
     return phoneme
